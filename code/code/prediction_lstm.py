770d21ee0851ad388d34d514cb371b309d2f17fb
Abdul Rehman Liaqat <liaqat@uni-hildesheim.de>
2018-07-31 09:57:36 +0200

    separate executable for prediction based models

---
+++ code/code/prediction_lstm.py
@@ -0,0 +1,18 @@
+from keras.models import Sequential, Model
+from keras.layers import Conv1D, Flatten, Dropout, Dense
+from utility import read_data,train_prediction_based_models,use_whole_data, write_result
+from models import prediction_lstm
+import os
+import pickle
+
+cwd = os.getcwd()
+path = cwd + "/data"
+data_files = read_data(path)
+window_size = 10
+nb_epoch = 20
+nb_features = 1
+model = prediction_lstm(window_size,nb_features)
+result_files = use_whole_data(data_files,window_size,nb_features,train_prediction_based_models,model)
+with open('prediction_lstm_results.obj','wb') as f:
+    pickle.dump(result_files,f)
+write_result(algorithm_name='prediction_lstm',data_files=result_files,results_path=cwd+'/results')
