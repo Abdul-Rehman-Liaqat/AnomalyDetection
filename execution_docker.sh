d5a85e5fc1807b2b2732e44116ce83ce12f7203b
Abdul Rehman Liaqat <liaqat@uni-hildesheim.de>
2018-08-03 18:11:36 +0200

    create automatic docker execution file

---
+++ execution_docker.sh
@@ -0,0 +1,1 @@
+docker run --user=root -p 8888:8888 -v $(pwd)/code/code:/executor/code abdulrehmanliaqat/execution_docker
---
+++ scoring_docker.sh
@@ -0,0 +1,2 @@
+docker run  -v $(pwd)/code/code/results:/usr/local/src/NAB/results  -v $(pwd)/code/code/data:/usr/local/src/NAB/data   -v $(pwd)/code/code/config:/usr/local/src/NAB/config -it abdulrehmanliaqat/scoring_docker bash
+
