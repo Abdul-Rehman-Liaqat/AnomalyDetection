116b4e696ebb8f3a467bc93553f652b1762e147d
Abdul Rehman Liaqat <liaqat@uni-hildesheim.de>
2018-08-09 15:55:42 +0200

    get reconstruction loss of autoencoder based models

--- code/code/utility.py
+++ code/code/utility.py
@@ -140,8 +140,9 @@ def train_autoencoder_based_models(df,model,input_shape,probation_period,nb_epoc
     for i in np.arange(input_shape[0]+1,len(df)):
         X_input = df["value"].values[i-(1+input_shape[0]):i-1].reshape((1,)+input_shape)
         pred = model.predict(X_input)   
-        prediction.append(model.predict(X_input))        
-        error_prediction.append((prediction[-1] -Y_input)[0][0])
+        prediction.append(pred)
+        error_prediction.append(np.sqrt((pred-X_input)*(pred-X_input))[0][0])
+        prediction.append(model.predict(X_input))      
         history = model.fit(X_input,Y_input , nb_epoch=nb_epoch, verbose=0)
         L.append(score_postprocessing(error_prediction,len(error_prediction)))
 #        print(i)
