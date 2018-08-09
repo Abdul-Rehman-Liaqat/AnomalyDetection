22efe115c27762c85fed85143589f53816276706
Abdul Rehman Liaqat <liaqat@uni-hildesheim.de>
2018-08-09 15:34:37 +0200

    setup prediction NN training

--- code/code/predictionNn.py
+++ code/code/predictionNn.py
@@ -9,11 +9,11 @@ cwd = os.getcwd()
 path = cwd + "/data"
 data_files = read_data(path)
 window_size = 10
-nb_epoch = 80
+nb_epoch = 1
 nb_features = 1
 input_shape = (window_size,)
 model = predictionNn(input_shape)
-result_files = use_whole_data(data_files,input_shape,train_prediction_based_models,model,nb_epoch=nb_epoch      )
+result_files = use_whole_data(data_files,input_shape,train_prediction_based_models,model,nb_epoch=nb_epoch)
 with open('predictionNn_results.obj','wb') as f:
     pickle.dump(result_files,f)
 write_result(algorithm_name='predictionNn',data_files=result_files,results_path=cwd+'/results')
