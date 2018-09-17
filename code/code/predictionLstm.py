817359bb72cb00d7f669339ceb94ac1fb2a0256d
Abdul Rehman Liaqat <liaqat@uni-hildesheim.de>
2018-09-17 17:33:51 +0200

    apply common code and parameter store capability

--- code/code/predictionLstm.py
+++ code/code/predictionLstm.py
@@ -1,25 +1,17 @@
-from keras.models import Sequential, Model
-from keras.layers import Conv1D, Flatten, Dropout, Dense
-from utility import read_data,train_prediction_based_models,use_whole_data, write_result
+from utility import read_data,train_prediction_based_models,use_whole_data, write_result, common_code , store_param
 from models import predictionLstm
 import os
-import pickle
-from datetime import datetime
-
-now = datetime.now()
-
 
 cwd = os.getcwd()
-path = cwd + "/data"
-config_path = cwd + "/config/data.config"
-data_files = read_data(path)
 window_size = 10
-nb_epoch = 20
+nb_epoch = 1
 nb_features = 1
 input_shape = (window_size,nb_features)
 model = predictionLstm(input_shape)
-result_files = use_whole_data(data_files,input_shape,train_prediction_based_models,model,config_path=config_path)
-algo_name = "predictionLstmOneEpoch{}{}{}{}".format(now.month,now.day,now.hour,now.minute)
-with open("dump/"+algo_name+".obj",'wb') as f:
-    pickle.dump(result_files,f)
+data_files,add_to_name = common_code()
+algo_type = "predictionLstm"
+algo_name = algo_type + add_to_name
+print(algo_name)
+result_files = use_whole_data(data_files,input_shape,train_prediction_based_models,model,config_path='config/data.config')
 write_result(algorithm_name=algo_name,data_files=result_files,results_path=cwd+'/results')
+store_param(window_size,nb_epoch,input_shape,algo_type,algo_name,model)
