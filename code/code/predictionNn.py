c1c187bee82ae74c99778c5c71aa01a7fa16c7b4
Abdul Rehman Liaqat <liaqat@uni-hildesheim.de>
2018-09-20 05:59:37 +0200

    add data configuration command line parser and storage

--- code/code/predictionNn.py
+++ code/code/predictionNn.py
@@ -8,10 +8,10 @@ nb_epoch = 1
 nb_features = 1
 input_shape = (window_size,)
 model = predictionNn(input_shape)
-data_files,add_to_name = common_code()
-result_files = use_whole_data(data_files,input_shape,train_prediction_based_models,model,nb_epoch=nb_epoch)
+data_files,add_to_name, data_config = common_code()
+result_files = use_whole_data(data_files,input_shape,train_prediction_based_models,model,nb_epoch=nb_epoch, data_config=data_config)
 algo_type = "predictionNnOneEpoch"
 algo_name = algo_type + add_to_name
 print(algo_name)
 write_result(algorithm_name=algo_name,data_files=result_files,results_path=cwd+'/results')
-store_param(window_size,nb_epoch,input_shape,algo_type,algo_name,model)
+store_param(window_size,nb_epoch,input_shape,algo_type,algo_name,model,data_config)
