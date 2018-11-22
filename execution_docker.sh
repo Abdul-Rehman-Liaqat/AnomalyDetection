0944fb6e068bc0428c48ebc64bc52d637c846463
Abdul Rehman Liaqat <liaqat@uni-hildesheim.de>
2018-11-22 20:54:14 +0100

    make additional repo

--- execution_docker.sh
+++ execution_docker.sh
@@ -1,1 +1,1 @@
-docker run --user=root -p 8888:8888 -v $(pwd)/code/code:/executor/code abdulrehmanliaqat/execution_docker
+docker run --user=root -p 8888:8888 -v $(pwd)/code/code:/executor/code/code abdulrehmanliaqat/execution_docker
